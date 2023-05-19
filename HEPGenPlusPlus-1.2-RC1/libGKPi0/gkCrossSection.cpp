2deeb0b657c1ecaa458256f34e79e903ee5469b1
Andrey <kenjo@jlab.org>
2023-05-19 14:02:30 -0400

    improve gkXS bin

--- HEPGenPlusPlus-1.2-RC1/libGKPi0/gkCrossSection.cpp
+++ HEPGenPlusPlus-1.2-RC1/libGKPi0/gkCrossSection.cpp
@@ -4,10 +4,10 @@
 #include <ctime>
 #include <fstream>
 
-#include <string>
 #include <sys/stat.h>
 #include <unistd.h>
 
+#include <TString.h>
 #include "libGKPi0.h"
 #include "gkSubProcessTable.h"
 
@@ -17,42 +17,58 @@ bool exists_test (const std::string& name) {
 }
 
 int main (int argc, char** argv) {
-  if (argc < 4) {
-    printf("Usage: %s [qsq] [xbj] [t] [phi] (-I [interpolationCacheIndex])\n",argv[0]);
-    printf("Alternate Usage: %s [qsq] [W] [t] [phi] -a (-I [interpolationCacheIndex])\n",argv[0]);
-    printf("Give phi in radians ;) \n");
+  string fileNameInterpolation;
+  if(exists_test(argv[argc-1]) && TString(argv[argc-1]).Contains("tableIndex")) {
+    fileNameInterpolation = argv[argc-1];
+    argc--;
+  }
+
+  std::vector<double> kins;
+  for(int iarg=1;iarg<argc;iarg++) {
+    TString arg(argv[iarg]);
+    if(arg.Contains("-newmix")) {
+      GKPI0::set_new_eta_mixing_angle(true);
+    } else if(arg.Contains("-176")) {
+      GKPI0::set_mu_eta(1.76);
+    } else if(arg.Contains("-eta")) {
+      GKPI0::SetReactionPar(2);
+    } else if(arg.Contains("-neutron")) {
+      GKPI0::SetReactionPar(3);
+    } else {
+      kins.push_back(atof(argv[iarg]));
+    }
+  }
+
+  double qsq, w2, xbj, phi=0, tt=-2;
+  if(kins.size()>=2) {
+    qsq = kins[0];
+    xbj = kins[1];
+    w2 = kins[1];
+
+    if(kins.size()==3) {
+      tt = kins[2];
+    } else if(kins.size()==4) {
+      phi = kins[3];
+    }
+  } else {
+    std::cerr<<Form("Usage: %s [-newmix] [-176] [-eta] [-neutron] qsq xbj [t] [phi] [interpolationCacheIndex]\n",argv[0]);
+    std::cerr<<Form("Give phi in radians ;) \n");
     return -1;
   }
 
-  double qsq=atof(argv[1]);
-  bool useInterPolation = false;
-  double phi = atof(argv[4]);
-
-  double w2 = atof(argv[2]);
-  double xbj = atof(argv[2]);
   if(w2<1) {
     w2 = GKPI0::getWsq(qsq,xbj);
   } else {
     xbj = GKPI0::getXbj(qsq,w2);
   }
 
-  string fileNameInterpolation;
-  if (argc > 6) {
-    useInterPolation = true;
-    fileNameInterpolation = argv[argc-1];
-  }
-
-
   GKPI0::amplitude myAmp;
-  double tt = atof(argv[3]);
   double xi = GKPI0::compassxi(qsq, xbj);
   double tmin = GKPI0::getTmin(qsq, xbj);
 
   double tprime = tt-tmin;
   printf("Final parameters: Q^2 %.4f, W %.4f, xbj %.4e, xi %.4e,t %.4f t' %.4f, phi %.4f\n",qsq,w2,xbj,xi,tt,tprime,phi);
-  if (!useInterPolation){
-
-    char buffer[200];
+  if (fileNameInterpolation.empty()) {
     //check if file with buffered subprocess amplitudes exist
     std::string myPath="";
     if (getenv("HEPGEN_CREATESPA") != NULL)
@@ -60,22 +76,24 @@ int main (int argc, char** argv) {
     else
       myPath ="./";
 
-    sprintf(buffer,"%s/preparation_%.4f_%.4f.dat",myPath.c_str(),qsq,w2);
+    TString fprep(Form("%s/preparation_%.4f_%.4f.dat",myPath.c_str(),qsq,w2));
 
-    if (exists_test(buffer)) // if so, load it
-      GKPI0::loadPreparationFromFile(buffer,qsq,xi);
+    if (exists_test(fprep.Data())) // if so, load it
+      GKPI0::loadPreparationFromFile(fprep.Data(),qsq,xi);
     else { //else we calculate them new
       GKPI0::prepareConvolution(qsq,xi);
-      GKPI0::savePreparationToFile(buffer); //and we save them, for the next run
+      GKPI0::savePreparationToFile(fprep.Data()); //and we save them, for the next run
     }
 
     //finally calculate amplitudes
     myAmp =  GKPI0::getAmplitude(qsq,xi,xbj,tt);
   } else {
+
     gkSubProcessTableCache myCache;
     myCache.loadCache(fileNameInterpolation);
     myAmp = myCache.getAmpsForKine(qsq,xbj,tt);
   }
+
 
   printf("Amplituden: \n");
   std::cout << "Mmmp0 Mmpp0 Mpmp0 Mppp0" << std::endl;
